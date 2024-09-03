06f5090ab0a5d00622b608c0b840e5b2020bd731
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-09-04 02:33:51 +0530

    Update index.jsx

--- client/src/service/axios/index.jsx
+++ client/src/service/axios/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const Axios=axios.create({baseURL:"http://localhost:8000/api/v1/"});
-const Axios=axios.create({baseURL:"https://inv-plt.onrender.com"});
+const Axios=axios.create({baseURL:"https://inv-plt.onrender.com/api/v1/"});
 
 export const Server='https://inv-plt.onrender.com'
 export default Axios;
