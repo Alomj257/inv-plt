123b910c9cc48f22820da0a8a3802f34a648c79e
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-09-04 23:58:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = require("./config/expressConfig");
 const http = require("http");
 const PORT = process.env.PORT || 8000;
 Connection();
-console.log(process.env.SECRET)
 
+console.log(process.env.EMAIL, process.env.PASS);
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
