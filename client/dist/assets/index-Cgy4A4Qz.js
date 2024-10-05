c62c9984dc9803e54c1ab16cd422b277618250f7
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-10-05 19:03:00 +0530

    depl

---
+++ client/dist/assets/index-Cgy4A4Qz.js
@@ -0,0 +1,1 @@
+import{w as a}from"./index-Dll_1HP0.js";const g=async s=>{const t=await a(s);try{return{totalInvestment:t==null?void 0:t.reduce((c,e)=>{var o;const m=((o=e==null?void 0:e.investors)==null?void 0:o.reduce((u,n)=>u+(parseInt((n==null?void 0:n.amount)||0)||0),0))||0;return c+m},0)}}catch(r){console.log(r)}};export{g};
